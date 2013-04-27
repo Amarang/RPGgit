1f66e0c2d4cd2cd653cf28347e66a32e7fb08f76
Nick Amin <typhoid.pwns@gmail.com>
2013-04-27 11:42:15 -0500

    Creating HUD
    Will graphically show player stats

---
+++ HUD.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.awt.*;
+import java.applet.*;
+import javax.swing.*;
+import java.util.*;
+import java.awt.image.*;
+import java.net.*;
+import javax.imageio.*;
+import java.util.Random;
+import java.awt.event.*;
+import java.lang.Math;
+import javax.sound.sampled.*;
+
+
+
+
+class HUD
+{
+    private int health;
+    private int healthmax;
+    private int mana;
+    private int strength;
+    private int speed;
+    private int damage;
+    private int defense;
+    private int gold;
+    private int experience;
+    private int level;
+	
+    
+    public HUD(Player p) {
+        this.p = p;
+    }
+    
+   
+
+}
